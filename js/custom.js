20e10645c10b4497d20eedcb0441f2812b3d4270
loukas pap <t8170101@aueb.gr>
2019-11-06 02:34:57 +0200

    evgala ta search apo to navbar, eftia3a ligo tiw selides kai eftia3a kapoia href

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+var url = document.location.toString();
+if (url.match('#')) {
+	$('.nav-tabs a[href="#' + url.split('#')[1] + '"]').tab('show');
+}
+
+$('.nav-tabs a').on('shown.bs.tab', function(e) {
+	window.location.hash = e.target.hash;
+})
